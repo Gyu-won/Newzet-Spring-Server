d45d09ec5df61e48b3fdca4c43f84017d66ee6d0
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-04-12 23:33:50 +0900

    Feat: RedisMutexFactory 생성
    기본적으로 interval 300초로 해서 polling 방식으로 mutex 대기 구현했지만, 추후 모니터링을 통해 부하가 너무 크면 pub/sub 방식으로 변경 예정

---
+++ src/main/java/com/newzet/api/common/mutex/RedisMutexFactory.java
@@ -0,0 +1,44 @@
+package com.newzet.api.common.mutex;
+
+import org.springframework.stereotype.Component;
+
+import com.newzet.api.common.cache.redis.RedisUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisMutexFactory implements MutexFactory{
+
+	private static final long MUTEX_ACQUISITION_INTERVAL_MS = 300;
+	private static final String MUTEX_PREFIX = "mutex:";
+	private static final String MUTEX_VALUE = "locked";
+
+	private final RedisUtil redisUtil;
+
+	@Override
+	public void tryMutex(String key, long waitTime, long leaseTime) {
+		long startTime = System.currentTimeMillis();
+		long endTime = startTime + waitTime;
+
+		while (System.currentTimeMillis() < endTime) {
+			boolean acquired = redisUtil.setMutex(MUTEX_PREFIX + key, MUTEX_VALUE, leaseTime);
+			if (acquired) return;
+
+			try {
+				Thread.sleep(MUTEX_ACQUISITION_INTERVAL_MS);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		throw new MutexAcquisitionException();
+	}
+
+	@Override
+	public void release(String key) {
+		redisUtil.delete(MUTEX_PREFIX + key);
+	}
+}
+
